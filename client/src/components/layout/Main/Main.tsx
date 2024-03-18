77c8cd51865169f659e6de7c3d041a5c4b057a29
Justyna Konikiewicz <zagorskaj@wp.pl>
2024-03-18 15:01:45 +0100

    add transition effect on cards

--- client/src/components/layout/Main/Main.tsx
+++ client/src/components/layout/Main/Main.tsx
@@ -4,15 +4,22 @@ const Main = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.card}>
+				<img src='./ball.jpg'></img>
 				<h2>Mecze</h2>
 			</div>
 			<div className={styles.card}>
+				<img src='./team.jpg'></img>
+
 				<h2>Zawodnicy</h2>
 			</div>
 			<div className={styles.card}>
+				<img src='./stats.jpg' alt='statistics'></img>
+
 				<h2>Statystyki</h2>
 			</div>
 			<div className={styles.card}>
+				<img src='./background.jpeg'></img>
+
 				<h2>O nas</h2>
 			</div>
 		</div>
