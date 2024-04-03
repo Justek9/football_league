d9ba760c104057cfe108cbdb44f149d91a6d317c
Justyna Konikiewicz <zagorskaj@wp.pl>
2024-04-03 20:03:29 +0200

    start working on mobile navigation

--- client/src/components/layout/Navigation/Navigation.tsx
+++ client/src/components/layout/Navigation/Navigation.tsx
@@ -1,23 +1,36 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Navigation.module.scss'
 
-const Navigation: FC = () => {
+const Navigation = () => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen)
+	}
+
 	return (
-		<ul className={styles.navigationContainer}>
-			<li>
-				<Link to='/zawodnicy'>Zawodnicy</Link>
-			</li>
-			<li>
-				<Link to='/mecze'>Mecze</Link>
-			</li>
-			<li>
-				<Link to='/o-nas'>O nas</Link>
-			</li>
-			<li>
-				<Link to='/statystyki'>Statystyki</Link>
-			</li>
-		</ul>
+		<>
+			<div className={`${styles.hamburgerIcon} ${isOpen ? styles.active : ''}`} onClick={toggleMenu}>
+				<div className={styles.bar1}></div>
+				<div className={styles.bar2}></div>
+				<div className={styles.bar3}></div>
+			</div>
+			<ul className={`${styles.navigationContainer} ${isOpen ? styles.menuOpen : ''}`}>
+				<li>
+					<Link to='/zawodnicy'>Zawodnicy</Link>
+				</li>
+				<li>
+					<Link to='/mecze'>Mecze</Link>
+				</li>
+				<li>
+					<Link to='/o-nas'>O nas</Link>
+				</li>
+				<li>
+					<Link to='/statystyki'>Statystyki</Link>
+				</li>
+			</ul>
+		</>
 	)
 }
 
